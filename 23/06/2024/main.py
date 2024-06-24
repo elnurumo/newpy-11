b0403b4d2af178c87857b8cf6745b1fb90764de3
elnurumo <elnurqasimov592@gmail.com>
2024-06-24 14:52:53 +0400

    Dövr ve if else

---
+++ 23/06/2024/main.py
@@ -0,0 +1,44 @@
+# saat1 = int(input())
+# a = saat1 *3600 + deqiqe1 * 60 + saniye1
+# print(saat1)
+
+
+# 66 Dövr dersi
+# n = int(input())
+# n = str(n)
+# for i in n:
+#     print(i)
+
+# n = int(input()) # 2565
+# b = []
+# while n > 0:
+#     k = n%10
+#     b.append(k)
+#     n = n // 10
+# print(b)
+# for i in range(len(b)-1, -1,-1):
+#     print(b[i])
+
+
+# # 66 İf else mövzusu
+# Email = 'email@inbox.ru'
+# password = '321abc'
+# EmailUser = input('Email: ')
+# passwordUser = input('Password: ')
+# if EmailUser == Email and passwordUser == password:
+#     print('welcome')
+# else:
+#     print('Email ve ya password yanlışdır')
+
+
+
+# # 67
+# n = int(input('n='))
+# b = int(input('b='))
+# s = 0
+# if b !=0:
+#     for i in range(1, n):
+#         if i**2 % b == 0:
+#             s = s + 1
+#     print('ədədlərin sayı',s)
+
